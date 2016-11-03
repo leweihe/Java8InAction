244f4a3f0f8b36a89f1f3eac26646692daa2e224
helewei <Lewei.He@linde-china.com>
2016-11-03 11:51:21 +0800

    add some exs

--- src/main/java/lambdasinaction/chap6/Partitioning.java
+++ src/main/java/lambdasinaction/chap6/Partitioning.java
@@ -1,6 +1,8 @@
 package lambdasinaction.chap6;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.*;
@@ -8,10 +10,12 @@ import static lambdasinaction.chap6.Dish.menu;
 
 public class Partitioning {
 
-    public static void main(String ... args) {
+    public static void main(String... args) {
         System.out.println("Dishes partitioned by vegetarian: " + partitionByVegeterian());
         System.out.println("Vegetarian Dishes by type: " + vegetarianDishesByType());
         System.out.println("Most caloric dishes by vegetarian: " + mostCaloricPartitionedByVegetarian());
+        System.out.println("All calories: " + sumAllCalories());
+        System.out.println("All dishes name: " + getShortMenuCommaSeparated());
     }
 
     private static Map<Boolean, List<Dish>> partitionByVegeterian() {
@@ -25,9 +29,16 @@ public class Partitioning {
     private static Object mostCaloricPartitionedByVegetarian() {
         return menu.stream().collect(
                 partitioningBy(Dish::isVegetarian,
-                        collectingAndThen(
-                                maxBy(comparingInt(Dish::getCalories)),
-                                Optional::get)));
+                        collectingAndThen(maxBy(comparingInt(Dish::getCalories)), Optional::get)));
+    }
+
+    private static Integer sumAllCalories() {
+        return menu.stream().collect(summingInt(Dish::getCalories));
+    }
+
+
+    private static String getShortMenuCommaSeparated() {
+        return menu.stream().map(Dish::getName).collect(joining(", "));
     }
 }
 
